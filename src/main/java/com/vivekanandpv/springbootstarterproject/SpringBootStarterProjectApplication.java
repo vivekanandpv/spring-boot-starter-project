0f2419d049d2e0210959c3bd32f29fd0cec4ca4c
vivek <vivekanandpv@outlook.com>
2023-03-23 16:57:13 +0530

    Initial commit

---
+++ src/main/java/com/vivekanandpv/springbootstarterproject/SpringBootStarterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootstarterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivekanandpv/springbootstarterproject/SpringBootStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootstarterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
